69988cfc1e3731116fcc0d0bfca83d40e904b9a9
siki2-uiuc <siki2@illinois.edu>
2022-10-10 15:48:43 +0000

    fire signs done

--- config/routes.rb
+++ config/routes.rb
@@ -17,13 +17,13 @@ Rails.application.routes.draw do
   # Uncomment each route below ONE AT A TIME and debug.
   # Do NOT uncomment more than one at a time, or you'll be dealing with multiple syntax errors at once.
 
-  get("/zodiacs/aries", { :controller =>  fire, action =>  "ram" })
+  get("/zodiacs/aries", { :controller =>  "fire", :action =>  "ram" })
   get("/zodiacs/leo", { :controller => "fire", :action => "lion" })
-  get("/zodiacs/sagittarius" { :controller => "fire", :action => "archer" })
+  get("/zodiacs/sagittarius", { :controller => "fire", :action => "archer" })
   
-  # get("/zodiacs/taurus", { :controller, "earth", :action, "bull" })
-  # get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
-  # get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
+  get("/zodiacs/taurus", { :controller, "earth", :action, "bull" })
+  get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
+  get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
   
   # get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
   # get(" /zodiacs/libra", { :controller => "air", :action => "scales" })
